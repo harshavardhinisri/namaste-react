7b75b10fb95a94a7a70ea615034da4ead0481a30
Harshavardhini Sridhar <harshavardhini.sri99@gmail.com>
2024-07-02 15:57:07 -0500

    Moved code till Episode 8 to git

---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import LOGO_URL from "../utils/constants";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+  console.log("Header rendered");
+  useEffect(() => {
+    console.log("useEffect rendered");
+  }, [btnNameReact]);
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact us</Link>
+          </li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              if (
+                btnNameReact === "Login"
+                  ? setBtnNameReact("Logout")
+                  : setBtnNameReact("Login")
+              );
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
