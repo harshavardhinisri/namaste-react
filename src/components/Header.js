c5778dd13e6c53ab62a5a301518601aee8011608
Harshavardhini Sridhar <harshavardhini.sri99@gmail.com>
2024-07-08 16:59:31 -0500

    Tailwind css - Episode 10

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,27 +8,27 @@ const Header = () => {
   const onlineStatus = useOnlineStatus();
   useEffect(() => {}, [btnNameReact]);
   return (
-    <div className="header">
+    <div className="flex justify-between bg-yellow-50 shadow-lg m- sm:bg-pink-50">
       <div className="logo-container">
-        <img className="logo" src={LOGO_URL} />
+        <img className="w-40" src={LOGO_URL} />
       </div>
-      <div className="nav-items">
-        <ul>
-          <li>Online Status: {onlineStatus ? "🟢" : "🔴"}</li>
-          <li>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          <li className="px-4">Online Status: {onlineStatus ? "🟢" : "🔴"}</li>
+          <li className="px-4">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/about">About us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/contact">Contact us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/grocery">Grocery</Link>
           </li>
 
-          <li>Cart</li>
+          <li className="px-4">Cart</li>
           <button
             className="login"
             onClick={() => {
